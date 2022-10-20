703a8d9ca207720d8bf9ee220ad7d2d17a09a891
ae-antares <t.ares@me.com>
2022-10-20 07:58:45 +0700

    ### 1.0.0 - 2010.2022 * Initial commit. * Project Clone

---
+++ log-version.md
@@ -0,0 +1,3 @@
+### 1.0.0 - 2010.2022
+* Initial commit.
+* Project Clone 
---
+++ src/main/java/com/idr/dev/model/ess_MISC.java
@@ -0,0 +1,12 @@
+package com.idr.dev.model;
+
+import lombok.Data;
+
+public @Data class ess_MISC {
+    private Long ID;
+    private String RFU_01;
+    private String RFU_02;
+    private String RFU_03;
+    private String RFU_04;
+    private String RFU_05;
+}
